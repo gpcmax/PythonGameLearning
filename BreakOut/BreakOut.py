0649f0ac3127f53126c3ae3008ab1991439c9ada
Gage <gage.copelin@gmail.com>
2019-05-11 21:30:25 -0400

    Update BreakOut.py

--- BreakOut/BreakOut.py
+++ BreakOut/BreakOut.py
@@ -60,6 +60,60 @@ class Paddle(GameObject):
             if self.ball is not None:
                     self.ball.move(offset, 0)
 
+class Brick(GameObject):
+    COLORS ={1: '#999999',2: '#555555',3: '#222222'}
+
+    def __int__(self, canvas, x, y, hits):
+        self.width = 75
+        self.height = 20
+        self.hits = hits
+        color = Brick.COLORS[hits]
+        item = canvas.create_rectangle(x - self.width / 2,
+                                       y - self.height / 2,
+                                       x + self.width / 2,
+                                       y + self.height / 2,
+                                       fill=color, tags='brick')
+        super(Brick,self).__init__(canvas,item)
+
+        def hit(self):
+            self.hits -=1
+            if self.hits == 0:
+                self.delete()
+            else:
+                self.canvas.itemconfig(self.item,
+                                       fill=Break.COLORS[self.hits])
+
+class Game(tk.Frame):
+    def __init__(self, master):
+        super(Game, self).__init__(master)
+        self.lives = 3
+        self.width = 610
+        self.height = 400
+        self.canvas = tk.Canvas(self, bg='#aaaaff',
+                                width=self.width,
+                                height=self.height)
+        self.canvas.pack()
+        self.pack()
+
+        self.items = {}
+        self.ball = None
+        self.paddle = Paddle(self.canvas, self.width/2, 326)
+        self.items[self.paddle.item] = self.paddle
+        for x in range(5, self.width - 5, 75):
+            self.add_brick(x + 37.5, 50, 2)
+            self.add_brick(x + 37.5, 70, 1)
+            self.add_brick(x + 37.5, 90, 1)
+
+        self.hud = None
+        self.setup_game()
+        self.canvas.focus_set()
+        self.canvas.bind('<Left>',
+                         lambda _: self.paddle.move(-10))
+        self.canvas.bind('<Right>',
+                         lambda _: self.paddle.move(10))
+
+        
+
 if __name__ == '__main__':
     root = tk.Tk()
     root.title('Hello Pong!')
